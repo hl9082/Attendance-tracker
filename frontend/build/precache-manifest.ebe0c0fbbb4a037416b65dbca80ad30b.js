d1edfa240a995fbfd17638babe7676c8be7d812a
huyisme-005 <legiahuy050104@gmail.com>
2024-12-22 22:48:54 -0500

    build added

---
+++ frontend/build/precache-manifest.ebe0c0fbbb4a037416b65dbca80ad30b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10b1ad066f7510a801538ddaae80582",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "4e7e1dc2dec2a365a226",
+    "url": "/my-app/static/css/main.dfd9bd17.chunk.css"
+  },
+  {
+    "revision": "4e7e1dc2dec2a365a226",
+    "url": "/my-app/static/js/main.3d48bac5.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
