0c7b8cea05a1659aeba0c31a8267f39956d4f557
huyisme-005 <legiahuy050104@gmail.com>
2024-12-23 09:40:50 -0500

    index.js updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@
  * @description Entry point for react app
  */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';  // Import global CSS styles (if any)
 import App from './App';  // Import the root App component
 import reportWebVitals from './reportWebVitals';  // Optional for measuring performance (if needed)
