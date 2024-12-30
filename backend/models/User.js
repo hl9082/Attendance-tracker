6caa247bb6c3fb9aae24a28e0a20daa07d937814
huyisme-005 <legiahuy050104@gmail.com>
2024-12-30 17:10:52 -0500

    Attendance updated

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,6 @@
 // _models/User.js
 const { DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs'); // Import bcrypt for password hashing
 const sequelize = require('./Database');  // Import the sequelize instance
 
 const User = sequelize.define('User', {
@@ -33,7 +34,24 @@ const User = sequelize.define('User', {
     type: DataTypes.DATE,
     defaultValue: DataTypes.NOW,
   },
+  password: {  // Add password field to store hashed passwords
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
+
+// Hash password before saving to the database
+User.beforeCreate(async (user) => {
+  if (user.password) {
+    const salt = await bcrypt.genSalt(10);  // Generate a salt
+    user.password = await bcrypt.hash(user.password, salt);  // Hash the password before saving
+  }
+});
+
+// Method to compare input password with stored hashed password
+User.prototype.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);  // Compare hashed password
+};
 
 module.exports = User;
 
